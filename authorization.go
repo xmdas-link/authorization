c6fb40d486e97ab0c8c600f8cd524cee9afa857b
linzhou <linzhou@dcx.com>
2019-09-09 11:26:58 +0800

    从auth组件获取用户名与用户角色信息

--- authorization.go
+++ authorization.go
@@ -5,17 +5,18 @@ import (
 
 	"github.com/casbin/casbin/v2"
 	"github.com/gin-gonic/gin"
+	"github.com/xmdas-link/auth"
 )
 
 func NewAuthorizer(enforcer *casbin.Enforcer) gin.HandlerFunc {
-	auth := &BasicAuthorizer{enforcer}
+	authorizer := &BasicAuthorizer{enforcer}
 
 	return func(context *gin.Context) {
-		// apply auth
+		// apply authorizer
 		log.Printf("casbin middleware. ip: %v, fullpath: %v, uri: %v", context.ClientIP(), context.Request.URL.Path, context.Request.RequestURI)
 
-		if !auth.CheckPermission(context) {
-			auth.RequirePermission(context)
+		if !authorizer.CheckPermission(context) {
+			authorizer.RequirePermission(context)
 		}
 	}
 }
@@ -24,25 +25,29 @@ type BasicAuthorizer struct {
 	enforcer *casbin.Enforcer
 }
 
-func (auth *BasicAuthorizer) GetUserName(ctx *gin.Context) string {
-	// TODO get user name from gin.context
-	return ctx.DefaultQuery("username", "")
+func (authorize *BasicAuthorizer) GetUserName(ctx *gin.Context) string {
+	user := ctx.GetStringMapString(auth.CtxKeyAuthUser)
+	if username, ok := user["name"]; ok {
+		return username
+	}
+
+	return ""
 }
 
-func (auth *BasicAuthorizer) GetUserRole(ctx *gin.Context) string {
-	// TODO get user role from gin.context
-	return ctx.DefaultQuery("rolename", "admin")
+func (authorize *BasicAuthorizer) GetUserRole(ctx *gin.Context) string {
+	userRole := ctx.GetString(auth.CtxKeyUserRole)
+	return userRole
 }
 
-func (auth *BasicAuthorizer) RequirePermission(ctx *gin.Context) {
+func (authorize *BasicAuthorizer) RequirePermission(ctx *gin.Context) {
 	ctx.AbortWithStatus(403)
 }
 
-func (auth *BasicAuthorizer) CheckPermission(ctx *gin.Context) bool {
+func (authorize *BasicAuthorizer) CheckPermission(ctx *gin.Context) bool {
 	obj := ctx.Request.URL.Path
-	sub := auth.GetUserName(ctx)
+	sub := authorize.GetUserName(ctx)
 	act := ctx.Request.Method
 
-	allowed, _ := auth.enforcer.Enforce(sub, obj, act)
+	allowed, _ := authorize.enforcer.Enforce(sub, obj, act)
 	return allowed
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.12
 require (
 	github.com/casbin/casbin/v2 v2.0.2
 	github.com/gin-gonic/gin v1.4.0
+	github.com/jinzhu/gorm v1.9.10 // indirect
+	github.com/xmdas-link/auth v0.0.0-20190909025952-cdf499f0e29e // indirect
 )
